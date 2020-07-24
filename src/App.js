e5c889ee8a755d80213941b9cd33949e883709cc
bakhanx <hansol732@naver.com>
2020-07-24 17:12:52 +0900

    #1.0 react App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
+function App() {
+  return <HashRouter>
+    <Route path="/" component={Home}/>
+    <Route path="/About" component={About}/>
+  </HashRouter>
+}
+
+export default App;
