e35d9ea5fe9e28319dd61aa16d426e00c3ee9661
bakhanx <hansol732@naver.com>
2020-07-24 18:50:42 +0900

    Updates

---
+++ precache-manifest.9bd1a5e698d4c9e2e6e07e9863fd9695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ecab0ae408180c86770330181c0c8a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3759bedc31476070a7d3",
+    "url": "/movie_app/static/css/main.3ba30130.chunk.css"
+  },
+  {
+    "revision": "8e83cfb4f827c246a364",
+    "url": "/movie_app/static/js/2.f0a08c44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.f0a08c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3759bedc31476070a7d3",
+    "url": "/movie_app/static/js/main.3a3575da.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
